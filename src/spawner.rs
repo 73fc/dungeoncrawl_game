ed7c29546ee5f4cc201629e376b2fd15c0a33cb2
73fc <542591851@qq.com>
2024-07-16 01:36:48 -0700

    feature: add victory and loss state

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,10 +8,7 @@ pub fn spawn_player(esc: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
-        Health {
-            current: 20,
-            max: 20,
-        },
+        Health { current: 3, max: 3 },
     ));
 }
 
@@ -21,20 +18,38 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
         _ => orc(),
     };
 
-    ecs.push((
-        Enemy,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph,
-        },
-        MovingRandomly {},
-        Health {
-            current: hp,
-            max: hp,
-        },
-        Name(name),
-    ));
+    let test = rng.range(0, 9) % 2;
+    if test == 0 {
+        ecs.push((
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            MovingRandomly {},
+            Health {
+                current: hp,
+                max: hp,
+            },
+            Name(name),
+        ));
+    } else {
+        ecs.push((
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            ChasingPlayer {},
+            Health {
+                current: hp,
+                max: hp,
+            },
+            Name(name),
+        ));
+    }
 }
 
 fn goblin() -> (i32, String, FontCharType) {
@@ -43,4 +58,17 @@ fn goblin() -> (i32, String, FontCharType) {
 
 fn orc() -> (i32, String, FontCharType) {
     (2, "Orc".to_string(), to_cp437('o'))
+}
+
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push((
+        Item,
+        AmuletOYala,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('|'),
+        },
+        Name("Amulet of Yala".to_string()),
+    ));
 }
